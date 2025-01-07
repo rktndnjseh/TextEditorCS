20d40255209bcb5774760922bb5bf76d11a94ebd
rktndnjseh <150515419+rktndnjseh@users.noreply.github.com>
2025-01-07 12:05:38 +0900

    Add files via upload

---
+++ firstProject/GoogleDriveService.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Drive.v3;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+
+namespace firstProject
+{
+    public class GoogleDriveService
+    {
+        private static readonly string[] Scopes = { DriveService.Scope.DriveFile };
+        private static readonly string ApplicationName = "TextEditorCloud";
+
+        public static DriveService Initialize()
+        {
+            UserCredential credential;
+
+            using (var stream = new FileStream("credentials.json", FileMode.Open, FileAccess.Read))
+            {
+                string credPath = "token.json";
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    Scopes,
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore(credPath, true)).Result;
+                Console.WriteLine("Credential file saved to: " + credPath);
+            }
+
+            var service = new DriveService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = ApplicationName,
+            });
+
+            return service;
+        }
+        //구글 드라이브에 파일 업로드
+        public static string UploadFile(DriveService service, string filePath, string mimeType)
+        {
+            var fileMetadata = new Google.Apis.Drive.v3.Data.File()
+            {
+                Name = Path.GetFileName(filePath)
+            };
+
+            FilesResource.CreateMediaUpload request;
+            using (var stream = new FileStream(filePath, FileMode.Open))
+            {
+                request = service.Files.Create(fileMetadata, stream, mimeType);
+                request.Fields = "id";
+                request.Upload();
+            }
+
+            return request.ResponseBody.Id;
+        }
+        //구글 드라이브에 파일 다운로드
+        public static void DownloadFile(DriveService service, string fileId, string savePath)
+        {
+            var request = service.Files.Get(fileId);
+            using (var stream = new FileStream(savePath, FileMode.Create, FileAccess.Write))
+            {
+                request.MediaDownloader.ProgressChanged += progress =>
+                {
+                    if (progress.Status == Google.Apis.Download.DownloadStatus.Completed)
+                    {
+                        Console.WriteLine("Download complete.");
+                    }
+                };
+                request.Download(stream);
+            }
+        }
+       
+    }
+}
